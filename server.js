7e41dae797cfc0a965ccdd390f3c4c5837ed0251
SuttiGuy <122422912+SuttiGuy@users.noreply.github.com>
2023-07-17 21:58:45 +0700

    Restaurant

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require ("cors");
+const restaurantRouter = require("./router/restaurant.router");
+
+
+
 const sql = require("./models/db")
-const PORT = 5000;
+const PORT = 4000;
 
 //create service
-const app = express()
-
+const app = express();
 //USe middleware
-app.use(express())
+app.use(express());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
@@ -15,6 +18,8 @@ app.get("/" , (req,res)=> {
     res.send("<h1>This is a restaurant  API </h1>");
     //RESTful API for restaurant
 })
+
+app.use("/", restaurantRouter);
 
 app.listen(PORT, () =>{
     console.log("Server is running on http://localhost:" + PORT);
